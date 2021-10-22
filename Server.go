529d7982352005918e031dd5cb9f4da6c7b4cac3
telefon2232 <telefon2232@gmail.com>
2021-10-22 12:54:40 +0300

    first commit

---
+++ Server.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/securecookie"
+	"html/template"
+	"net/http"
+)
+
+type Hello struct {
+	Hi string
+}
+
+// cookie handling
+
+var cookieHandler = securecookie.New(
+	securecookie.GenerateRandomKey(64),
+	securecookie.GenerateRandomKey(32))
+
+func getUserName(request *http.Request) (userName string) {
+	if cookie, err := request.Cookie("session"); err == nil {
+		cookieValue := make(map[string]string)
+		if err = cookieHandler.Decode("session", cookie.Value, &cookieValue); err == nil {
+			userName = cookieValue["name"]
+		}
+	}
+	return userName
+}
+
+func setSession(userName string, response http.ResponseWriter) {
+	value := map[string]string{
+		"name": userName,
+	}
+	if encoded, err := cookieHandler.Encode("session", value); err == nil {
+		cookie := &http.Cookie{
+			Name:  "session",
+			Value: encoded,
+			Path:  "/",
+		}
+		http.SetCookie(response, cookie)
+	}
+}
+
+func clearSession(response http.ResponseWriter) {
+	cookie := &http.Cookie{
+		Name:   "session",
+		Value:  "",
+		Path:   "/",
+		MaxAge: -1,
+	}
+	http.SetCookie(response, cookie)
+}
+
+// login handler
+
+func loginHandler(response http.ResponseWriter, request *http.Request) {
+	name := request.FormValue("name")
+	pass := request.FormValue("password")
+	redirectTarget := "/"
+	if name != "" && pass != "" {
+		// .. check credentials ..
+		setSession(name, response)
+		redirectTarget = "/internal"
+	}
+	http.Redirect(response, request, redirectTarget, 302)
+}
+
+// logout handler
+
+func logoutHandler(response http.ResponseWriter, request *http.Request) {
+	clearSession(response)
+	http.Redirect(response, request, "/", 302)
+}
+
+// index page
+
+
+func indexPageHandler(response http.ResponseWriter, request *http.Request) {
+
+	var indexPage,err = template.ParseFiles("index.html")
+	if err!=nil{
+		fmt.Println(err)
+		return
+	}
+	indexPage.Execute(response,"")
+}
+
+// internal page
+
+
+
+func internalPageHandler(response http.ResponseWriter, request *http.Request) {
+	userName := getUserName(request)
+	if userName != "" {
+
+		var indexPage,err = template.ParseFiles("lk.html")
+		if err!=nil{
+			fmt.Println(err)
+			return
+		}
+		indexPage.Execute(response,Hello{userName})
+
+
+
+
+	} else {
+		http.Redirect(response, request, "/", 302)
+	}
+}
+
+// server main method
+
+var router = mux.NewRouter()
+
+func main() {
+
+	router.HandleFunc("/", indexPageHandler)
+	router.HandleFunc("/internal", internalPageHandler)
+
+	router.HandleFunc("/login", loginHandler).Methods("POST")
+	router.HandleFunc("/logout", logoutHandler).Methods("POST")
+
+	http.Handle("/", router)
+	http.ListenAndServe(":80", nil)
+}
